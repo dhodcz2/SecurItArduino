a18e3c6677e65928fd7be7b43bff0e476873d4d0
Daniel Hodczak <dhodcz2@uic.edu>
2021-03-09 11:18:04 -0600

    a

--- src/Snapshot.ino
+++ src/Snapshot.ino
@@ -18,6 +18,7 @@
 #include <Adafruit_VC0706.h>
 #include <SPI.h>
 #include <SD.h>
+#include <Arduino.h>
 
 #if defined(__AVR__) || defined(ESP8266)
 
@@ -78,40 +79,40 @@ void setup() {
 #if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
   if(chipSelect != 53) pinMode(53, OUTPUT); // SS on Mega
 #else
-  if(chipSelect != 10) pinMode(10, OUTPUT); // SS on Uno, etc.
+  if (chipSelect != 10) pinMode(10, OUTPUT); // SS on Uno, etc.
 #endif
 #endif
 
   Serial.begin(9600);
   Serial.println("VC0706 Camera snapshot test");
-  
+
   // see if the card is present and can be initialized:
   if (!SD.begin(chipSelect)) {
-    Serial.println("Card failed, or not present");
-    // don't do anything more:
-    return;
-  }  
-  
+	Serial.println("Card failed, or not present");
+	// don't do anything more:
+	return;
+  }
+
   // Try to locate the camera
   if (cam.begin()) {
-    Serial.println("Camera Found:");
+	Serial.println("Camera Found:");
   } else {
-    Serial.println("No camera found?");
-    return;
+	Serial.println("No camera found?");
+	return;
   }
   // Print out the camera version information (optional)
   char *reply = cam.getVersion();
   if (reply == 0) {
-    Serial.print("Failed to get version");
+	Serial.print("Failed to get version");
   } else {
-    Serial.println("-----------------");
-    Serial.print(reply);
-    Serial.println("-----------------");
+	Serial.println("-----------------");
+	Serial.print(reply);
+	Serial.println("-----------------");
   }
 
   // Set the picture size - you can choose one of 640x480, 320x240 or 160x120 
   // Remember that bigger pictures take longer to transmit!
-  
+
   cam.setImageSize(VC0706_640x480);        // biggest
   //cam.setImageSize(VC0706_320x240);        // medium
   //cam.setImageSize(VC0706_160x120);          // small
@@ -126,23 +127,23 @@ void setup() {
   Serial.println("Snap in 3 secs...");
   delay(3000);
 
-  if (! cam.takePicture()) 
-    Serial.println("Failed to snap!");
-  else 
-    Serial.println("Picture taken!");
-  
+  if (!cam.takePicture())
+	Serial.println("Failed to snap!");
+  else
+	Serial.println("Picture taken!");
+
   // Create an image with the name IMAGExx.JPG
   char filename[13];
   strcpy(filename, "IMAGE00.JPG");
   for (int i = 0; i < 100; i++) {
-    filename[5] = '0' + i/10;
-    filename[6] = '0' + i%10;
-    // create if does not exist, do not open existing, write, sync after write
-    if (! SD.exists(filename)) {
-      break;
-    }
+	filename[5] = '0' + i / 10;
+	filename[6] = '0' + i % 10;
+	// create if does not exist, do not open existing, write, sync after write
+	if (!SD.exists(filename)) {
+	  break;
+	}
   }
-  
+
   // Open the file for writing
   File imgFile = SD.open(filename, FILE_WRITE);
 
@@ -157,23 +158,24 @@ void setup() {
   // Read all the data up to # bytes!
   byte wCount = 0; // For counting # of writes
   while (jpglen > 0) {
-    // read 32 bytes at a time;
-    uint8_t *buffer;
-    uint8_t bytesToRead = min((uint16_t)32, jpglen); // change 32 to 64 for a speedup but may not work with all setups!
-    buffer = cam.readPicture(bytesToRead);
-    imgFile.write(buffer, bytesToRead);
-    if(++wCount >= 64) { // Every 2K, give a little feedback so it doesn't appear locked up
-      Serial.print('.');
-      wCount = 0;
-    }
-    //Serial.print("Read ");  Serial.print(bytesToRead, DEC); Serial.println(" bytes");
-    jpglen -= bytesToRead;
+	// read 32 bytes at a time;
+	uint8_t *buffer;
+	uint8_t bytesToRead = min((uint16_t) 32, jpglen); // change 32 to 64 for a speedup but may not work with all setups!
+	buffer = cam.readPicture(bytesToRead);
+	imgFile.write(buffer, bytesToRead);
+	if (++wCount >= 64) { // Every 2K, give a little feedback so it doesn't appear locked up
+	  Serial.print('.');
+	  wCount = 0;
+	}
+	//Serial.print("Read ");  Serial.print(bytesToRead, DEC); Serial.println(" bytes");
+	jpglen -= bytesToRead;
   }
   imgFile.close();
 
   time = millis() - time;
   Serial.println("done!");
-  Serial.print(time); Serial.println(" ms elapsed");
+  Serial.print(time);
+  Serial.println(" ms elapsed");
 }
 
 void loop() {
