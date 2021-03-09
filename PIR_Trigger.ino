9d7bd57bb1d48171499c133977c799e0d1987e7c
Daniel Hodczak <dhodcz2@uic.edu>
2021-03-09 10:42:26 -0600

    a

---
+++ PIR_Trigger.ino
@@ -0,0 +1,35 @@
+int pinRead = 7;
+int pinLed = 13;
+
+int pirState = LOW;
+int numValue = 0;
+
+void setup() {
+ 
+  pinMode(pinLed,OUTPUT);
+  pinMode(pinRead,INPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+ 
+  numValue = digitalRead(pinRead);
+
+  if (numValue == HIGH){
+    digitalWrite(pinLed, HIGH);
+    
+  if(pirState == LOW){
+    Serial.println("Motion detected");
+    pirState = HIGH;
+    }
+  }
+
+  else {
+    digitalWrite(pinLed, LOW);
+  if (pirState == HIGH){
+    Serial.println("Motion Ended");
+    pirState = LOW;
+  }
+  }
+}
